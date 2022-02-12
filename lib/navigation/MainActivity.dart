578eef90bb8b32eb8f1abbf96e34f19b5f226708
jiqingke <jiqingke@cvnavi.com>
2022-02-12 17:21:08 +0800

    增加导航

---
+++ lib/navigation/MainActivity.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(HomeApp());
+}
+
+class HomeApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
